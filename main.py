44a71966d799be0cf7ad365d1728ebb9138a01f0
lorisveron <loris.veron@insa-lyon.fr>
2025-12-19 10:09:06 +0100

    Modif sur les pop-up

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ data.columns = data.columns.str.strip() # ça permet de supprimer les espaces tr
 # Supprimer les colonnes vides et les colonnes "Unnamed"
 data = data.loc[:, ~data.columns.str.contains('Unnamed', na=False)]
 
-# data = data.head(100) A ACTIVER SI ON VEUT VISUALISER LA MAP
+data = data.head(100) # A ACTIVER SI ON VEUT VISUALISER LA MAP
 
 # Afficher les premières lignes du dataset
 print(data.head())
@@ -34,17 +34,31 @@ data_cleaned = data.drop_duplicates(keep='first')
 print(f"After removing duplicates: {len(data_cleaned)}")
 print(data.info())  # column names and data types
 
+data_cleaned.describe()
+
 # Créer une carte
 # On centre la carte sur une lat et long moyennes
 map_center = [data['lat'].mean(), data['long'].mean()]
 
-map_init = folium.Map(location=map_center, zoom_start=12)
+map_init = folium.Map(location=map_center, zoom_start=12, tiles='Esri.WorldImagery') # On change le fond de carte sur l'option Tiles
 
 for idx, row in data.iterrows():
+
+    popup_text = f"""
+    <b>User:</b> {row['user']}<br>
+    <b>Tags:</b> {row['tags']}<br>
+    <b>Title:</b> {row['title']}<br>
+    <b>Date Taken:</b> {row['date_taken_year']}-{row['date_taken_month']}-{row['date_taken_day']}
+    """
+
     folium.Marker(
         location=[row['lat'], row['long']],
-        popup=row['title'],
-        icon=folium.Icon(color='blue', icon='info-sign')
+        radius=1,
+        popup=folium.Popup(popup_text, max_width=300),
+        color='blue', 
+        fill=True,
+        fillColor='blue',
+        fillOpacity=0.6
     ).add_to(map_init)
 
 # Sauvegarder la carte pour la regarder après
